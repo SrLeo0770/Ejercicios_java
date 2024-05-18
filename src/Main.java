4aaefa59beca472c004102974b739a0cdfdba630
SrLeo0770 <bmartinezd3@miumg.edu.gt>
2024-05-17 21:41:36 -0600

    lo mismo que el ejercicio anterior, pero usando el bucle while

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        int numero = 0;
+        int numero;
 
-        while (true) {
-            System.out.print("Por favor, ingrese un número mayor a 10 y menor que 30: ");
-            if (scanner.hasNextInt()) {
-                numero = scanner.nextInt();
-                if (numero > 10 && numero < 30) {
-                    break;
-                } else {
-                    System.out.println("El número debe ser mayor a 10 y menor que 30. Intente de nuevo.");
-                }
-            } else {
-                System.out.println("Entrada no válida. Por favor, ingrese un número.");
-                scanner.next(); // Descarta la entrada no válida
-            }
+        do {
+            System.out.print("Escriba un número mayor a 10 y menor que 30: ");
+            numero = scanner.nextInt();
+        } while (numero <= 10 || numero >= 30);
+
+        int contador = 1;
+        while (contador <= numero) {
+            System.out.println(contador);
+            contador += 2;
         }
-
-        System.out.println("Números impares desde 1 hasta " + numero + ":");
-        for (int i = 1; i <= numero; i++) {
-            if (i % 2 != 0) {
-                System.out.println(i);
-            }
-        }
-
-        scanner.close();
     }
 }
