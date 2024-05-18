e2e712750b98421a2fb3e4d8a9a935de65b5857f
SrLeo0770 <bmartinezd3@miumg.edu.gt>
2024-05-17 21:27:26 -0600

    Factorial de un numero

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        int numero;
-        System.out.println("ingrese un numero");
+ int numero;
+        int factorial=1;
+        System.out.println("ingrese un numero:");
         numero = Integer.parseInt(System.console().readLine());
-        if (numero % 2 == 0) {
-            System.out.println(numero+" si es un numero par");
-        }else {
-            System.out.println(numero+" no es un numero par");
+        int i=numero;
+        while (i>1){
+            factorial*=i;
+            i--;
         }
+        System.out.println("El factorial de "+numero+" es: "+factorial);
     }
 }
