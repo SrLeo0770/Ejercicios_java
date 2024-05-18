316f86b138483512a6972812e3254f53f4a60027
SrLeo0770 <bmartinezd3@miumg.edu.gt>
2024-05-17 21:23:42 -0600

    Si un numero es par o impar

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        int edad;
-
-        float a,b;
-        System.out.println("Ingrese el primer numero: ");
-        a = Float.parseFloat(System.console().readLine());
-        System.out.println("Ingrese el segundo numero: ");
-        b = Float.parseFloat(System.console().readLine());
-        if(a>b){
-            System.out.println(a+" Es mayor que "+b);
+        int numero;
+        System.out.println("ingrese un numero");
+        numero = Integer.parseInt(System.console().readLine());
+        if (numero % 2 == 0) {
+            System.out.println(numero+" si es un numero par");
         }else {
-            System.out.println(b+"Es mayor que "+a);
+            System.out.println(numero+" no es un numero par");
         }
     }
 }
